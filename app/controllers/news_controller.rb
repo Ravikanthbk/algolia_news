9972fab12daaf47fde52f6756876fd63886b4f74
Ravikanth Nawada <ravikanthnawada@gmail.com>
2016-01-13 14:15:47 +0800

    Update news_controller.rb

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,5 @@
 require_relative '../../lib/services/algolia/algolia_service.rb'
 class NewsController < ApplicationController
-#require 'services/algolia/v1/algolia_service'
   def index
     search_news_list = V1::Algolia.new('v1').service
     @news = Kaminari.paginate_array(search_news_list["hits"], total_count: total_news_count(search_news_list)).page(params[:page]).per(5)
