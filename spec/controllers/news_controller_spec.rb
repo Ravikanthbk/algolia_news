cdcfdc11fac7c16a91564d8fceff586807db8ac5
Ravikanth Nawada <ravikanthnawada@gmail.com>
2016-01-13 16:01:24 +0800

    Update news_controller_spec.rb

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,6 +1,20 @@
+
 require 'spec_helper'
 require 'rails_helper'
-RSpec.describe NewsController do
+require_relative '../data_helper/data_helper'
+describe NewsController do
+  before do
+    @response = DataHelper.test_data
+  end
+  context 'GET #index' do
+    it 'should search for news' do
+      stub_request(:get, "http://hn.algolia.com/api/v1/search_by_date?numericFilters=points%3E=1000&query=github&restrictSearchableAttributes=url").
+        with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
+        to_return(:status => 200, :body => @response, :headers => {})
 
-
+      search_news_list = V1::Algolia.new('v1').service
+      expect(search_news_list["nbHits"]).to eq(10)
+      expect(search_news_list["page"]).to eq(0)
+    end
+  end
 end
